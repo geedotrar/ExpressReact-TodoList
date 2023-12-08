2b442ccc429b2b53200bedda3d89c51afd2c94f3
geedotrar <104245694+geedotrar@users.noreply.github.com>
2023-12-08 07:59:47 +0700

    Backend

--- db.js
+++ db.js
@@ -1,14 +1,14 @@
 const Pool = require("pg").Pool;
 
-// const pool = new Pool({
-//   user: "postgres",
-//   password: "admin",
-//   host: "localhost",
-//   port: 5432,
-//   database: "postgres",
-// });
 const pool = new Pool({
-  connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+  user: "postgres",
+  password: "admin",
+  host: "localhost",
+  port: 5432,
+  database: "postgres",
 });
+// const pool = new Pool({
+//   connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+// });
 
 module.exports = pool;
